44a61c162a490f0a11374eb59e36f2235b84fd10
Martin_Gabriel_Cobos_Trevino <a.21308051280342@cbtis128.edu.mx>
2024-04-10 15:32:04 +0000

    update

---
+++ lib/movies.dart
@@ -0,0 +1,21 @@
+import 'package:drawerv2/drawer_menu.dart';
+import 'package:flutter/material.dart';
+
+class Movies extends StatelessWidget {
+  static const String routeName = '/movies';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Movies"),
+      ),
+      drawer: DrawerMenu(),
+      body: Center(
+        child: Text(
+          'Movies Page',
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
